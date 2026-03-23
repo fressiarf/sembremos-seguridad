8dac4adbfb550faed9372477b2c370ff1ace5ca6
Andres Villalobos <avillaloboshfwdgmail.com>
2026-03-23 15:58:50 -0600

    Cambios

--- src/components/Dashboard/SeccionPrincipal/SeccionPrincipal.jsx
+++ src/components/Dashboard/SeccionPrincipal/SeccionPrincipal.jsx
@@ -11,6 +11,7 @@ import MapaRiesgos from '../MapaRiesgos/MapaRiesgos';
 import ZonasCriticas from '../ZonasCriticas/ZonasCriticas';
 import { useLogin } from '../../../context/LoginContext';
 
+import { FileText } from 'lucide-react';
 import Calendario from '../../Calendario/Calendario';
 
 // Mapeo de vistas a nombres de sección para el TopbarInstitucion
